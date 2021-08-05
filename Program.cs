66a7e79a24754bc5ffefaebe6118a803b60a652f
akhil2502 <86817237+akhil2502@users.noreply.github.com>
2021-08-05 18:18:48 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,10 +4,6 @@ namespace List
 {
     class Program
     {
-        /// <summary>
-        /// entry point of the application.
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args)
         {
             LinkedList linkedList = new LinkedList();

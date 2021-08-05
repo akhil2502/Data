3090c6f1649c6748fa00f20267f7d48f269467ee
akhil2502 <akhilachilies>
2021-08-05 18:11:45 +0530

    [akhil] Add:UC1-list

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace List
+{
+    class Program
+    {
+        /// <summary>
+        /// entry point of the application.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+            Console.WriteLine("----------------");
+            linkedList.Display();
+        }
+    }
+}

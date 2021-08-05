662d72d6b2027303b80d8f4ff644f3cee1d09081
akhil2502 <akhilachilies>
2021-08-05 18:52:19 +0530

    [akhil] Add:uc3-newelement

--- node.cs
+++ node.cs
@@ -6,8 +6,13 @@ namespace List
 {
     class Node
     {
+        // variables
         public int data;
         public Node next;
+        /// <summary>
+        /// constructor is used to initialized the data.
+        /// </summary>
+        /// <param name="data"></param>
         public Node(int data)
         {
             this.data = data;

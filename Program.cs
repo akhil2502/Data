c901ff76c69cc08b5ac29738c43bc784bbf16f34
akhil2502 <akhilachilies>
2021-08-08 21:42:53 +0530

    [akhil] Add:uc7

--- Program.cs
+++ Program.cs
@@ -4,15 +4,16 @@ namespace List
 {
     class Program
     {
+        /// <summary>
+        /// entry point of the application.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             LinkedList linkedList = new LinkedList();
             linkedList.Add(56);
             linkedList.Add(30);
             linkedList.Add(70);
-            linkedList.Display();
-            Console.WriteLine("----------------");
-            linkedList.PopLast();
             linkedList.Display();
         }
     }
